3a5230023e20e8e61aa347e7900163eff7e5858c
user1 <user1@DESKTOP-NC04KRC>
2024-12-09 17:10:55 +0900

    [ADD]

--- src/com/cookie/day08/oop/Exam_Access.java
+++ src/com/cookie/day08/oop/Exam_Access.java
@@ -3,8 +3,17 @@ package com.cookie.day08.oop;
  *  접근지정자(제한자)
  *  종류: public, protected, default, private
  */
+
+class Sample{
+	public int everyone;
+	private int onlyme;
+	int onlyus;
+}
 public class Exam_Access {
 	public static void main(String[] args) {
-		
+		Sample smp = new Sample();
+		smp.everyone = 1118;
+//		smp.onlyme = 0;
+		smp.onlyus = 502;
 	}
 }
